1e7d3115f6ee515a20fbb16e699510f93ada1667
Jolanda Verhoef <jolanda@returntothesource.nl>
2013-12-30 21:56:41 +0100

    basic home page with static image

--- django/project/urls.py
+++ django/project/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'wereldontdooien.views.home', name='home'),
     url(r'^editor/', include('ckeditor.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
