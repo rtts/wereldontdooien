4c4ea01a16b04be7b036cee8a8bda60970cf620d
Jolanda Verhoef <jolanda.verhoef@thenextview.nl>
2013-12-30 12:24:13 +0100

    WebView settings. Device back button is handled by the webview, links are not opened in the browser but in the app, javascript is enabled.

--- android/wereldontdooien/src/main/java/nl/returntothesource/wereldontdooien/MainActivity.java
+++ android/wereldontdooien/src/main/java/nl/returntothesource/wereldontdooien/MainActivity.java
@@ -2,10 +2,13 @@ package nl.returntothesource.wereldontdooien;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +16,23 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        final WebView contentView = (WebView) findViewById(R.id.fullscreen_content);
-        contentView.loadUrl("http://www.google.nl/");
+        myWebView = (WebView) findViewById(R.id.fullscreen_content);
+        myWebView.loadUrl("http://www.wereldontdooisters.nl/");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 
 }
