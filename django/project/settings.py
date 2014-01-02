1abaeaa19fda0a7e5e7a9039b6e8d1ee8667f61e
Jaap Joris Vens <jj@returntothesource.nl>
2014-01-02 20:50:59 +0100

    updated requirements

--- django/project/settings.py
+++ django/project/settings.py
@@ -53,7 +53,6 @@ LANGUAGE_CODE = 'nl'
 USE_I18N = True
 USE_L10N = True
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
-print LOCALE_PATHS
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "files"),)
