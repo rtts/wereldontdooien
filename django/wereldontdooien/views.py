9391c5100d04daf7e704ebb449d552bc9d6a7db2
Jaap Joris Vens <jj@returntothesource.nl>
2014-01-02 20:20:01 +0100

    fixed syntax errors

--- django/wereldontdooien/views.py
+++ django/wereldontdooien/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render, redirect
 from wereldontdooien.models import Fonkel
 
 def home(request):
-    [previous, current] = Fonkel.objects.filter(gepubliceerd__isnull=False)[:2]
+#todo try catch
+    [current, previous] = Fonkel.objects.order_by("-order").filter(gepubliceerd__isnull=False)[:2]
+        
     return render(request, "index.html", {
             "fonkel": current,
             "previous": previous,
             "next": False,
             })
 
-def moment(request, nr):
-    fonkel = get_object_or_404(Fonkel, order=nr)
+def fonkel(request, nr):
+    current = get_object_or_404(Fonkel, order=nr)
     try:
         previous = (Fonkel.objects
                     .order_by("-order")
                     .filter(gepubliceerd__isnull=False)
-                    .filter.(order__lt=nr)[0]
+                    .filter(order__lt=nr)[0]
                     )
     except Fonkel.DoesNotExist:
         previous = False
@@ -24,13 +26,13 @@ def moment(request, nr):
         next = (Fonkel.objects
                 .order_by("order")
                 .filter(gepubliceerd__isnull=False)
-                .filter.(order__gt=nr)[0]
+                .filter(order__gt=nr)[0]
                 )
     except Fonkel.DoesNotExist:
         next = False
 
     return render(request, "index.html", {
-            "fonkel": fonkel,
+            "fonkel": current,
             "previous": previous,
             "next": next,
             })
@@ -39,5 +41,5 @@ def random(request):
     fonkel = Fonkel.objects.order_by("?").filter(gepubliceerd__isnull=False)[0]
     return redirect(fonkel)
 
-def about(request):
+def info(request):
     render(request, "info.html", {})
