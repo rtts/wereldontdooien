8e19e894d399657e161defe3f386861719cebdba
Jaap Joris Vens <jj@returntothesource.nl>
2014-04-17 15:08:08 +0200

    trying to revert behavior, unsure to where

--- django/wereldontdooien/models.py
+++ django/wereldontdooien/models.py
@@ -21,7 +21,8 @@ class BaseFonkel(models.Model):
             ))
     tekst = models.CharField(max_length=1000)
     afbeelding = models.ImageField(upload_to=imgfile)
-    gebruiker = models.ForeignKey(User)
+    gebruiker = models.ForeignKey(User, blank=True, null=True) # blank = true makes testing easier
+)
 
     def __unicode__(self):
         return self.tekst
